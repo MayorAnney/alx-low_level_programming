86f66e0fa4347ff715e2ec938e288977b42b86a5
Mayor Anney <mayoranney11@gmail.com>
2022-09-08 15:08:18 -0700

    task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  *main- Serves as entry point for program
+ *   *
+ *    *Return: By default returns zero if no erros else returns non-zero value.
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
